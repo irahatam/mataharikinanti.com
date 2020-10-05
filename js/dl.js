a2365e031d641029c7429d911efa6aa81cb38f7d
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-10-05 16:49:53 -0400

    added new design league page w/ js

---
+++ js/dl.js
@@ -0,0 +1,34 @@
+let outputElement = document.getElementById("outputElement");
+let outputParagraph = document.getElementById("outputParagraph");
+let contentGridElement = document.getElementById("contentGrid");
+
+let jsonDatabase = [
+  {
+    img: "#",
+    title: "<br>Title",
+  },
+];
+
+for (var i = 0; i < jsonDatabase.length; i++) {
+  createElementProper(jsonDatabase[i]);
+}
+
+function createElementMessy(incomingJSON) {
+  var incompleteHTML =
+    '<div class="contentItem"><h3 class="contentTitle">' +
+    incomingJSON["title"];
+}
+
+function createElementProper(incomingJSON) {
+  let newContentElement = document.createElement("DIV");
+
+  let newImage = document.createElement("IMG");
+  newImage.classList.add("productPhoto");
+  newImage.src = incomingJSON["img"];
+  newContentElement.appendChild(newImage);
+  contentGridElement.appendChild(newContentElement);
+
+  let newContentHeading = document.createElement("h6");
+  newContentHeading.innerHTML = incomingJSON["title"];
+  newContentElement.appendChild(newContentHeading);
+}
